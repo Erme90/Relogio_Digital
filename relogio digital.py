f1d118fe4777d44de98a7f3d0d59b7a1c4bb40bb
Emerson Freire <emersonnascimento.freire@gmail.com>
2022-03-19 00:20:14 -0300

    Refatoração das variáveis

--- relogio digital.py
+++ relogio digital.py
@@ -1,41 +1,39 @@
-from calendar import WEDNESDAY
-from locale import LC_TIME, setlocale
 from tkinter import *
 import pyglet
 from datetime import datetime
 
-
+pyglet.font.add_file('ninepin.ttf')
 pyglet.font.add_file('digital-7.ttf')
 
-bc = '#ffffff'
-fc = '#ff0000'
+corback = '#fffffe'
+corfoot = '#ff000f'
 
-#configuração da janela
+#Configuração de janela
 janela = Tk()
 janela.title('Relógio digital')
-janela.geometry('450x150')
-janela.resizable (width=False, height=False)
-janela.configure(bg=bc)
+janela.geometry('290x75')
+janela.resizable(width=False, height=False)
+janela.configure(bg=corback)
 
 def relogio():
-    time = datetime.now()
-    hora = time.strftime('%H : %M : %S')
-    diasemana = time.strftime('%A - ')
-    dia = time.day
-    mes = time.strftime('%b')
-    ano = time.strftime('%Y')
-    relogio.configure(text=hora)
-    relogio.after(200, clock)
-    calendario.configure(text=diasemana + ' ' + str(dia) + '/ ' + 
-    str(mes) + '/ ' + str(ano))
+    tempo = datetime.now()
+    hora = tempo.strftime('%H: %M: %S')
+    semana = tempo.strftime('%a')
+    dia = tempo.day
+    mes = tempo.strftime('%b')
+    ano = tempo.strftime('%Y')
+    horario.configure(text=hora)
+    horario.after(200,relogio)
+    data.configure(text=semana + ' - ' + str(dia)+ '/' + str(mes)
+    + '/'+ str(ano))
+    
 
-relogio = Label(janela, font=('Arial 60'),bg=bc, fg = fc)
-relogio.grid(row=0, column=0, padx=5, sticky= NW)
+horario = Label(janela,text='23:36:00', font=('ninepin 20'), 
+          bg=corback,fg=corfoot)
+horario.grid(row=0, column=0, padx=10, pady=7, sticky=NW)
 
-calendario = Label(janela,font=('Arial 20'),bg=bc)
-calendario.grid(row=1, column=0, padx=15, sticky=NW)
+data = Label(janela,text='Friday - 18/03/2022', font=('ninepin 12'), bg=corback, fg=corfoot)
+data.grid(row=1, column=0,padx=10, sticky=NW)
 relogio()
-
-
 janela.mainloop()
 
